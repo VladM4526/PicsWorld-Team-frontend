d409413d3027ef7c1e7daf076dbd58b9b13b8eb3
VladM <vdstories20@gmail.com>
2023-12-26 17:02:02 +0100

    fix page

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ export const App = () => {
   return (
     <>
       <Routes>
-        <Route path="/WelcomePage" element={<Layout />}>
+        <Route path="/" element={<Layout />}>
           <Route index path="/WelcomePage" element={<WelcomePage />} />
           <Route index path="/FormLogin" element={<FormLogin />} />
           <Route index path="/FormReg" element={<FormReg />} />
