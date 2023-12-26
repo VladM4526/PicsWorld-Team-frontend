e939a7247c7cb215310b2b17f7f96fe8a897830f
VladM <vdstories20@gmail.com>
2023-12-26 21:10:15 +0100

    set up backend

---
+++ src/Redux/API/api.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+
+const baseURL = 'https://water-tracker-backend.onrender.com/api';
+
+axios.defaults.baseURL = baseURL;
+
+const setToken = token => {
+  baseURL.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+  localStorage.setItem('token', token);
+};
+
+// const removeToken = () => {
+//   delete baseURL.defaults.headers.common['Authorization'];
+//   localStorage.removeItem('token');
+// };
+
+export const signup = async body => {
+  const { data } = await baseURL.post('/auth/signup', body);
+  setToken(data.token);
+  return data;
+};
+
+export const signin = async body => {
+  const { data } = await baseURL.post('/auth/signin', body);
+  setToken(data.token);
+  return data;
+};
+
+export const refreshUser = async token => {
+  setToken(token);
+  const { data } = await baseURL.get('/users/current');
+  return data;
+};
