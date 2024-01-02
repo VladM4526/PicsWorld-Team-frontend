a6c736d7c4ef6a1020d6001fc220d5c37372395e
Volodymyr <buvoro@gmail.com>
2024-01-02 18:17:32 +0200

    Fix

--- src/Components/FormLogin/FormLogin.js
+++ src/Components/FormLogin/FormLogin.js
@@ -1,15 +1,15 @@
 // import { BgPicture } from './BgPicture';
 import { FormSignIn } from './Form';
-import { Header } from '../WelcomePage/Header/Header';
+// import { Header } from '../WelcomePage/Header/Header';
 import { FormSection, ImgBottle } from './Form.styled';
 
 export const FormLogin = () => {
   return (
     <>
-      <Header />
+      {/* <Header /> */}
       <ImgBottle>
         <FormSection>
-      {/* <BgPicture /> */}
+          {/* <BgPicture /> */}
           <FormSignIn />
         </FormSection>
       </ImgBottle>
