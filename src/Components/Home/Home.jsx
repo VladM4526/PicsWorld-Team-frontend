4df167a9b56e235bf8b8b61d33e3c6b3a3876ad2
Bakkara <50306713+bakkara@users.noreply.github.com>
2024-01-06 00:57:20 +0200

    fix home styles

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,16 +1,22 @@
 import { Today } from './Today';
 import { DailyTrackerWrapStyled, HomeWrapStyled } from './Home.styled';
 import WaterRatioPanel from 'Components/WaterRatioPanel/WaterRatioPanel';
+import { ContainerStyled } from 'Components/Layout/Layout.styled';
+import DailyNorma from 'Components/DailyNorma/DailyNorma';
 
 export const Home = () => {
   return (
-    <HomeWrapStyled>
-      <DailyTrackerWrapStyled>
-        <div style={{ minWidth: '280px', height: '134px' }}>
-          <WaterRatioPanel />
-        </div>
-      </DailyTrackerWrapStyled>
+    <ContainerStyled>
+      <HomeWrapStyled>
+     
+        <DailyTrackerWrapStyled>
+          <DailyNorma />
+          <div style={{ minWidth: '280px', height: '134px' }}>
+            <WaterRatioPanel />
+          </div>
+        </DailyTrackerWrapStyled>
       <Today />
-    </HomeWrapStyled>
+      </HomeWrapStyled>
+    </ContainerStyled>
   );
 };
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,23 +1,23 @@
-import { NavLink } from 'react-router-dom';
-import {
-  LogoApp,
-  LogoContainer,
-} from '../Components/WelcomePage/Header/Header.styled';
-import WaterTracker from '../img/set-icons.svg';
-import DailyNorma from 'Components/DailyNorma/DailyNorma';
+// import { NavLink } from 'react-router-dom';
+// import {
+//   LogoApp,
+//   LogoContainer,
+// } from '../Components/WelcomePage/Header/Header.styled';
+// import WaterTracker from '../img/set-icons.svg';
 import { Home } from 'Components/Home/Home';
+import { Header } from 'Components/WelcomePage/Header/Header';
 
 export const HomePage = () => {
   return (
     <>
-      <LogoContainer>
+      {/* <LogoContainer>
         <NavLink to="/HomePage">
           <LogoApp>
             <use href={`${WaterTracker}#icon-logo-app`}></use>
           </LogoApp>
         </NavLink>
-      </LogoContainer>
-      <DailyNorma />
+      </LogoContainer> */}
+      <Header/>
       <Home />
     </>
   );
