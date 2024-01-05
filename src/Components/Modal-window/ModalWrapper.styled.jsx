6ca6d5049f70d290b652a76a4a66409c50934a7f
Bakkara <50306713+bakkara@users.noreply.github.com>
2024-01-05 22:31:40 +0200

    Update ModalWrapper.styled.jsx

--- src/Components/Modal-window/ModalWrapper.styled.jsx
+++ src/Components/Modal-window/ModalWrapper.styled.jsx
@@ -4,22 +4,28 @@ export const ModalWrapStyled = styled.div`
   position: fixed;
   top: 0;
   left: 0;
-  width: 100%;
-  height: 100%;
+  width: 100vw;
+  height: 100vh;
   background-color: rgb(0, 0, 0, 0.8);
+  padding-top: 20px;
+  padding-bottom: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 export const ModalWindowStyled = styled.div`
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
+  /* position: absolute; */
+  /* top: 50%;
+  left: 50%; */
+  /* transform: translate(-50%, -50%); */
   width: 280px;
-  height: auto;
+  max-height: 100%;
   padding: 24px 12px;
   color: #2f2f2f;
   background-color: #fff;
   border-radius: 10px;
+  overflow-y: scroll;
   @media only screen and (min-width: 768px) {
     width: 704px;
     padding: 32px 24px;
