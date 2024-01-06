6137da84d7a56ee42d5fee315d4fdc6290e63b32
Bakkara <50306713+bakkara@users.noreply.github.com>
2024-01-06 02:33:14 +0200

    add percentage

--- src/Components/WaterRatioPanel/WaterRatioPanel.jsx
+++ src/Components/WaterRatioPanel/WaterRatioPanel.jsx
@@ -1,8 +1,28 @@
+import { useDispatch } from "react-redux"
 import { PanelContainer, WaterPanel, WaterPercent} from "./WaterRatioPanel.styled"
+import { useWater } from "redux-files/hooks/useWater"
+import { useEffect } from "react"
+import { fetchWater } from "redux-files/water/waterOperations"
 
 
 const WaterRatioPanel = () => {
-    const percentage = 40
+  const { waterNotes } = useWater ()
+  console.log(waterNotes[0].percentage)
+  const dispatch = useDispatch()
+  const percentage = parseInt(waterNotes[0].percentage)
+  console.log(percentage)
+
+   useEffect (() => {
+    const fetchData = async () => {
+      try {
+        await dispatch(fetchWater ());
+      } catch (error) {
+        console.error('Error getting water data in useEffect:', error);
+      }
+    };
+
+    fetchData();
+  }, [dispatch]);
   return (
     <PanelContainer>
           <p>Today</p>
